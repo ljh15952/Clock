70960bfb69097c7d4dded691d0c89dce73f72be8
ljh15952 <ljh15952@gmail.com>
2025-03-20 11:25:32 +0900

    refactoring

--- src.js
+++ src.js
@@ -1,23 +1,30 @@
 
 function updateTime() {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");  // 9시 → "09"
-    const minutes = String(now.getMinutes()).padStart(2, "0"); // 5분 → "05"
-    const seconds = String(now.getSeconds()).padStart(2, "0"); // 3초 → "03"
-    document.getElementById("time").innerText = hours + " : " + minutes + " : " + seconds;
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
 
-    const h = document.getElementById("target-hour").value;
-    const m = document.getElementById("target-minute").value;
+    const h1 = String(hour).padStart(2, "0");  // 9시 → "09"
+    const m1 = String(minute).padStart(2, "0"); // 5분 → "05"
+    const s1 = String(second).padStart(2, "0"); // 3초 → "03"
+    document.getElementById("current-time").innerText = `현재 시간: ${h1}:${m1}:${s1}`;
 
-    const S1 = (now.getHours() * 60 * 60) + (now.getMinutes() * 60) + now.getSeconds()
-    const S2 = (h * 60 * 60) + (m * 60)
+    const t_h = document.getElementById("target-hour").value;
+    const t_m = document.getElementById("target-minute").value;
+
+    if (t_h == '' || t_m == '')
+        return;
+
+    const S1 = (hour * 60 * 60) + (minute * 60) + second
+    const S2 = (t_h * 60 * 60) + (t_m * 60)
     const diff = (S2 - S1) + (24 * 60 * 60)
-    const h1 = Math.floor(diff / 3600) % 24
-    const m1 = Math.floor(diff / 60) % 60
-    const s1 = diff % 60
+    const h2 = Math.floor(diff / 3600) % 24
+    const m2 = Math.floor(diff / 60) % 60
+    const s2 = diff % 60
 
-    document.getElementById("remaining-time").innerText = 
-    `남은 시간: ${h1}:${m1}:${s1}`;
+    document.getElementById("remaining-time").innerText =
+        `남은 시간: ${h2}:${m2}:${s2}`;
 
 }
 
